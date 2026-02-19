084390b794592ceaa5c13ebff1512fe9c211cfa1
Hafijur Rahman <hafijurrahman@Hafijurs-MacBook-Air.local>
2026-02-19 14:54:10 +0600

    made login button interactive

---
+++ script/login.js
@@ -0,0 +1,18 @@
+document.getElementById('login-btn')
+    .addEventListener('click', function(){
+        const numberInput = document.getElementById('number-input').value;
+        document.getElementById('number-input').value = '';
+        const pinInput = document.getElementById('pin-input').value;
+        document.getElementById('pin-input').value = '';
+        
+        if(numberInput === '01786444587' && pinInput === '5875'){
+            alert('Login Success');
+            // window.location.replace("home.html");
+            window.location.assign("home.html");
+        }
+        else{
+            alert('Login Failed');
+            return;
+        }
+
+    })
