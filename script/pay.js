b2b23fdc08a1e6abdcd8c5a7ddeb4c157b251cad
Hafijur Rahman <hafijurrahman@Hafijurs-MacBook-Air.local>
2026-02-21 19:51:34 +0600

    pay bill section completed with interective

--- script/pay.js
+++ script/pay.js
@@ -1,8 +1,8 @@
 document.getElementById("pay-btn")
     .addEventListener("click", function(){
-        const bank = getValueFromInput("select-to-pay");
-        console.log(bank)
-        if(bank === "Select to pay bill"){
+        const bill = getValueFromInput("select-to-pay");
+        
+        if(bill === "Select to pay bill"){
             alert("Please select to pay bill");
             return;
         }
@@ -12,8 +12,22 @@ document.getElementById("pay-btn")
             return;
         }
         const payAmount = getValueFromInput("pay-amount");
+        // console.log(payAmount)
         if(getCurrentBalance() < Number(payAmount)){
             alert("Insufficient Balance");
             return;
+        }
+        else if(payAmount === ''){
+            alert("Please set the amount");
+            return;
+        }
+        const pin = getValueFromInput("pay-pin");
+        if(pin === '1234'){
+            const newBalance = getCurrentBalance() - Number(payAmount);
+            alert(`${bill} ${payAmount} taka paid by ${accountNumber} at ${new Date()}`);
+            setBalance(newBalance);
+        }
+        else{
+            alert('Wrong pin');
         }
     })
